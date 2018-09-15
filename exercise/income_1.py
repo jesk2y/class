25a764907d6a90dff7806d045062e808ba33480e
DESKTOP-VUFA1P2\rnrmf <spdl9793@naver.com>
2018-09-15 19:55:05 +0900

    no message

--- exercise/income_1.py
+++
@@ -1,8 +0,0 @@
-people_count=int(input())
-movie_outcome=20
-ticket=5
-people_outcome=0.5
-income=people_count*ticket
-outcome=movie_outcome+(people_outcome*people_count)
-total_income=income-outcome
-print(total_income)
--- exercise/recursive_1.py
+++
@@ -1,8 +0,0 @@
-def doA(num): # 순수함수
-    if num == 10:
-        return num
-
-    print("doA", num)
-    return doA(num+1) #바깥에 변수 선언 안해도 됨 -> 메모리 안꼬임
-    #안정적
-print(doA(1))
